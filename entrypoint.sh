8105cb5397b7f809b7f8f52500702a15e7072cae
Felipe Suero <felipesu19@github.com>
2021-10-07 11:04:28 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 while true; do 
     echo $INPUT_STATUS_URL
     RESP=$(curl --header "Authorization: Bearer $INPUT_TOKEN" $INPUT_STATUS_URL)
-    echo $( jq .status "$RESP")
+    echo $( jq .status <<< "$RESP")
     # if [ .jq .status "$RESP" == '{ "status": "deployment_queued" }' ]; then
     #     echo "this works!"
     #     break
