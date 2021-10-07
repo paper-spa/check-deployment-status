78de7c1f153fc9c8a4ba3a6a28034268bd4cf5f7
Felipe Suero <felipesu19@github.com>
2021-10-07 12:14:30 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 i=1
 while true; do 
     RESP=$(curl --header "Authorization: Bearer $INPUT_TOKEN" $INPUT_STATUS_URL)
-    STATUS=$(jq -n "$RESP" | jq .status)
+    STATUS=$(jq -n "$RESP" | jq .status | xargs)
     if [ "$STATUS" = "succeed" ]; then
-        echo "Build finished, with status!"
+        echo "Build finished, with status $STATUS!"
         exit 0
     else
         echo "Current status is $STATUS"
@@ -14,5 +14,5 @@ while true; do
         exit 1
     fi
     i=$((i+1))
-    sleep 1
+    sleep 10
 done 
