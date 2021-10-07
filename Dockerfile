cf607e8c2c408e972c41d05297238bc4ccc44658
Felipe Suero <felipesu19@github.com>
2021-10-07 11:50:58 -0400

    lets make it bash, its easier

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM curlimages/curl:latest
 COPY . /app
 USER root
 #needed to parse response
-RUN apk add --no-cache  jq
+RUN apk add --no-cache  jq bash
 
 ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 while true; do 
     echo $INPUT_STATUS_URL
     i=1
     RESP=$(curl --header "Authorization: Bearer $INPUT_TOKEN" $INPUT_STATUS_URL)
     STATUS=$(jq -n "$RESP" | jq .status)
-    if [[ "$STATUS" == "succeed" ]]; then
+    if [[ $STATUS == "succeed" ]]; then
         echo "Build finished, with status!"
-        break
+        exit 0
     else
         echo "Current status is $STATUS"
     fi
-    if (($i == 10)); then
+    if [ $i -eq 10 ]; then
         echo "Build failed!"
         exit 1
     fi
