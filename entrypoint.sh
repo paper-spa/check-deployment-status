f6dde4edbc8cfeffc4624254161265ccf0fbcce1
Felipe Suero <felipesu19@github.com>
2021-10-07 10:00:31 -0400

    Add authorization layer

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,15 @@
 
 while true; do 
     echo $INPUT_STATUS_URL
-    RESP=$(curl -I $INPUT_STATUS_URL)
+    RESP=$(curl --header "Authorization: Bearer $INPUT_TOKEN" $INPUT_STATUS_URL)
     if [ "$RESP" == "1" ]; then
         echo "1"
         break
     fi
     echo "test: "
     echo $RESP
+    echo $INPUT_TOKEN
+    echo $INPUT_STATUS_URL
     sleep 1 
     break 
 done 
