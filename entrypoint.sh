56fb66448bec130af344a2d6d404a9104d9b5862
Mingzi <yimysty@github.com>
2021-10-08 10:21:01 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ while true; do
     RESP=$(curl --header "Authorization: Bearer $INPUT_TOKEN" $INPUT_STATUS_URL)
     STATUS=$(jq -n "$RESP" | jq .status | xargs)
     if [ "$STATUS" = "succeed" ]; then
-        echo "Build finished, with status $STATUS!"
+        echo "Deployment finished, with status $STATUS!"
         exit 0
     else
         echo "Current status is $STATUS"
     fi
     if [ $i -eq $INPUT_TIMEOUT ]; then
-        echo "Build failed!"
+        echo "Deployment failed!"
         exit 1
     fi
     i=$((i+1))
