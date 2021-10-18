9e74430f6f1a3fb40dd43673d659f0661470072d
Felipe Suero <felipesu19@github.com>
2021-10-18 13:20:10 -0400

    javascript action

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+async function check() {
+    try {
+        const api_token = core.getInput('token');
+        const url = core.getInput('status_url');
+        const timeout = core.getInput('timeout');
+        var tries = 0;
+        while (tries < timeout) {
+            tries++;
+            await new Promise(r => setTimeout(r, 1000));
+            var res = await fetch(url, {
+                headers: {
+                    'Authorization': `token ${api_token}`
+                }
+            });
 
-try {
-    const api_token = core.getInput('token');
-    const url = core.getInput('status_url');
-    const timeout = core.getInput('timeout');
-    var tries = 0;
-    while (tries < timeout) {
-        tries++;
-        await new Promise(r => setTimeout(r, 1000));
-        var res = await fetch(url, {
-            headers: {
-                'Authorization': `token ${api_token}`
+            if (res.status == 200) {
+                console.log(res);
+                break;
             }
-        });
-
-        if (res.status == 200) {
-            console.log(res);
-            break;
         }
+    } catch (error) {
+        core.setFailed(error.message);
     }
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+check();
