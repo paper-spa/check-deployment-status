a77a4ca0c3cad203a37af3f8927661eacd9aa299
Felipe Suero <felipesu19@github.com>
2021-10-18 13:17:02 -0400

    javascript action

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM curlimages/curl:latest
-COPY . /app
-USER root
-#needed to parse response
-RUN apk add --no-cache  jq bash
-
-ENTRYPOINT ["/app/entrypoint.sh"]
---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const api_token = core.getInput('token');
+    const url = core.getInput('status_url');
+    const timeout = core.getInput('timeout');
+    var tries = 0;
+    while (tries < timeout) {
+        tries++;
+        await new Promise(r => setTimeout(r, 1000));
+        var res = await fetch(url, {
+            headers: {
+                'Authorization': `token ${api_token}`
+            }
+        });
+
+        if (res.status == 200) {
+            console.log(res);
+            break;
+        }
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
